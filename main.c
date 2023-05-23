daac9913c0fdfafedafff89c836dd70425c83c3d
Johan Sebastian Navarro Duarte <johnavar@c4a3c8.42berlin.de>
2023-05-23 19:01:11 +0200

    parse some flags

--- main.c
+++ main.c
@@ -6,31 +6,37 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 00:41:33 by johnavar          #+#    #+#             */
-/*   Updated: 2023/05/21 21:26:37 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/05/23 19:00:54 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "include/ft_printf.h"
+#include <stdint.h>
 
 int	main(void)
 {
 	int	count;
 	int	num;
 
-	count = ft_printf("hello %s\n", "me");
-	ft_printf("The chars written in ft_printf are %d\n", count);
-	ft_printf("Now on Hexadecimal %x or %X\n", count, count);
-	count = ft_printf("%d\n", INT32_MIN);
-	ft_printf("The chars written in ft_printf are %d\n", count);
-	ft_printf("Now on Hexadecimal %x or %X\n", count, count);
-	count = printf("%d\n", INT32_MIN);
-	printf("The chars written in printf are %d\n", count);
-	printf("Now on Hexadecimal %x or %X\n", count, count);
-	count = ft_printf("%u\n", UINT32_MAX);
-	ft_printf("The chars written in ft_printf are %d\n", count);
-	count = printf("%u\n", UINT32_MAX);
-	printf("The chars written in printf are %d\n", count);
-	ft_printf("The address of the counter is %p\n", &count);
-	printf("The address of the counter is %p\n", &count);
+	count = 2;
+	printf("print 2 =>%-99d\n", count);
+	printf("print 2 =>%-99x\n", count);
+	printf("print 2 =>%-99s\n", "hello");
+	printf("print 2 =>%-99p\n", &count);
+	/*count = ft_printf("hello %s\n", "me");*/
+	/*ft_printf("The chars written in ft_printf are %d\n", count);*/
+	/*ft_printf("Now on Hexadecimal %x or %X\n", count, count);*/
+	/*count = ft_printf("%d\n", INT32_MIN);*/
+	/*ft_printf("The chars written in printf are %d\n", count);*/
+	/*ft_printf("Now on Hexadecimal %x or %X\n", count, count);*/
+	/*count = printf("%d\n", INT32_MIN);*/
+	/*printf("The chars written in printf are %\n", count);*/
+	/*printf("Now on Hexadecimal %x or %X\n", count, count);*/
+	/*count = ft_printf("%u\n", UINT32_MAX);*/
+	/*ft_printf("The chars written in ft_printf are %d\n", count);*/
+	/*count = printf("%u\n", UINT32_MAX);*/
+	/*printf("The chars written in printf are %d\n", count);*/
+	/*ft_printf("The address of the counter is %p\n", &count);*/
+	/*printf("The address of the counter is %p\n", &count);*/
 	return (count);
 }
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 16:01:25 by johnavar          #+#    #+#             */
-/*   Updated: 2023/05/22 15:20:21 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/05/23 19:00:45 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,16 @@ t_print	initialize_tab(void)
 
 char	*parse_flags(const char *format, t_print *flags)
 {
-
+	while ((*(++format) || ft_isflag(*(format)) || ft_isdigit(*(format)))
+		&& !ft_isspec(*(format)))
+	{
+		if (*(format) == '-')
+			flags->left = 1;
+		if (*(format) == '0')
+			flags->zero = 1;
+		if (*(format) == '.')
+			flags->prc = 1;
+	}
 }
 
 int	ft_printf(const char *format, ...)
@@ -69,8 +78,8 @@ int	ft_printf(const char *format, ...)
 		if (*format == '%' && *(format + 1) != '\0')
 		{
 			flags = initialize_tab();
-			format = parse_flags(++format, &flags);
-			count += print_spec(*(++format), ap);
+			format = parse_flags(format, &flags);
+			count += print_spec(*(format), ap);
 		}
 		else
 			count += write(1, format, 1);
